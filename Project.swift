e217906b7b8a8e070108c3f7bb134372e637ef4c
decanus <7621705+decanus@users.noreply.github.com>
2019-10-20 11:37:57 +0200

    done

--- Project.swift
+++ Project.swift
@@ -1,34 +1,38 @@
 import ProjectDescription
-  
-let project = Project(name: "Trollbox",
-                      targets: [
-                        Target(name: "Trollbox",
-                               platform: .iOS,
-                               product: .app,
-                               bundleId: "io.ultralightbeam.trollbox",
-                               infoPlist: "Info.plist",
-                               sources: ["Sources/**"],
-                               resources: [
-                               ],
-                               dependencies: [
-                          	    .cocoapods(path: ".")
-                                ]),
-                        Target(name: "TrollboxTests",
-                               platform: .iOS,
-                               product: .unitTests,
-                               bundleId: "io.ultralightbeam.trollboxTests",
-                               infoPlist: "Tests.plist",
-                               sources: "Tests/**",
-                               dependencies: [
-                                    .target(name: "Trollbox")
-                               ]),
-			Target(name: "TrollboxUITests",
-                               platform: .iOS,
-                               product: .uiTests,
-                               bundleId: "io.ultralightbeam.trollboxUITests",
-                               infoPlist: "UITests.plist",
-                               sources: "UITests/**",
-                               dependencies: [
-                                    .target(name: "Trollbox")
-                               ])
-                      ])
+
+let project = Project(
+    name: "Trollbox",
+    targets: [
+        Target(
+            name: "Trollbox",
+            platform: .iOS,
+            product: .app,
+            bundleId: "io.ultralightbeam.trollbox",
+            infoPlist: "Info.plist",
+            sources: ["Sources/**"],
+            resources: [],
+            dependencies: [
+                .cocoapods(path: ".")
+            ]),
+        Target(
+            name: "TrollboxTests",
+            platform: .iOS,
+            product: .unitTests,
+            bundleId: "io.ultralightbeam.trollboxTests",
+            infoPlist: "Tests.plist",
+            sources: "Tests/**",
+            dependencies: [
+                .target(name: "Trollbox")
+            ]),
+        Target(
+            name: "TrollboxUITests",
+            platform: .iOS,
+            product: .uiTests,
+            bundleId: "io.ultralightbeam.trollboxUITests",
+            infoPlist: "UITests.plist",
+            sources: "UITests/**",
+            dependencies: [
+                .target(name: "Trollbox")
+            ])
+    ]
+)
